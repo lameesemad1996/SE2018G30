1fe87dbcaa1472272d2755e7971c8366e90f07c2
Matt <matttppp@gmail.com>
2019-02-14 22:12:14 +0200

    last push from mac

--- ListenTogether/app/RoomServer.py
+++ ListenTogether/app/RoomServer.py
@@ -2,11 +2,32 @@ import asyncore
 import socket
 from threading import Thread
 
+
 class CommunicationHandler(asyncore.dispatcher_with_send):
+
+    def __init__(self, sock, room):
+        asyncore.dispatcher_with_send.__init__(self, sock)
+        self.room = room
+
     def handle_read(self):
         data = self.recv(8192)
         if data:
-            self.send(data)
+            data_string = data.decode('ascii')
+            msg = ""
+            if data_string == 'resume':
+                self.room.player_state = 1
+                msg = str(self.room.player_state)
+            elif data_string == 'pause':
+                self.room.player_state = 0
+                msg = str(self.room.player_state)
+            elif data_string == 'state':
+                msg = str(self.room.player_state)
+            elif data_string.__contains__('seek'):
+                parts = data_string.partition(" ")
+                self.room.time = parts[1]
+            elif data_string == 'time':
+                msg = self.room.time
+            self.send(msg.encode('ascii'))
 
 
 class RoomServer(asyncore.dispatcher):
@@ -18,6 +39,9 @@ class RoomServer(asyncore.dispatcher):
         self.bind((host, port))
         self.listen(5)
         self.port = port
+        self.player_state = 0
+        self.url = ""
+        self.time = 0
         print("Created a room server")
 
     def handle_accept(self):
@@ -25,13 +49,15 @@ class RoomServer(asyncore.dispatcher):
         if pair is not None:
             sock,addr = pair
             print('Incoming connection from %s' % repr(addr))
-            handler = CommunicationHandler(sock)
+            handler = CommunicationHandler(sock, room=self)
 
 
-def asyncoreLoop():
+
+def asyncore_loop():
     while True:
         asyncore.loop()
 
-t1 = Thread(target=asyncoreLoop)
+
+t1 = Thread(target=asyncore_loop)
 t1.setDaemon(True)
 t1.start()
