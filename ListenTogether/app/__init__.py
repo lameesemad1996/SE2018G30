83c1136020b4be63d3aab1d5c95d939aaddf96bd
Matt <matttppp@gmail.com>
2019-02-15 23:37:59 +0200

    returned ip

--- ListenTogether/app/__init__.py
+++ ListenTogether/app/__init__.py
@@ -10,4 +10,4 @@ from app.MainRequestHandler import *
 from app.StorageModule import *
 
 #if __name__ == "__main__":
-app.run(debug=True, host="18.191.35.199", port=80)
+app.run(debug=True, host="0.0.0.0", port=80)
